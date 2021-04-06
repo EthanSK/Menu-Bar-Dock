3c7cc908d47de932a4702551e739e9b16bb5e34f
Ethan SK <ethaneus99@gmail.com>
2021-04-07 00:34:29 +0100

    all menu bar dock

--- MenuBarDockTests/MenuBarDockTests.swift
+++ MenuBarDockTests/MenuBarDockTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class MenuDockTests: XCTestCase {
+class MenuBarDockTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- MenuBarDockUITests/MenuBarDockUITests.swift
+++ MenuBarDockUITests/MenuBarDockUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class MenuDockUITests: XCTestCase {
+class MenuBarDockUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
